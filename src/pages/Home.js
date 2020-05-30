4aa238f104b00e3d4f2f2ce9059ada6b66d226d6
daviddyess <david.dyess@gmail.com>
2020-05-30 17:29:31 -0600

    Updated game server addresses

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,10 +12,10 @@ class Home extends Component {
         <h3 className="text-center mt-2">Game Servers</h3>
         <Row>
           <Col>
-            <CSSServerStatus server={{ host: 'css.paradyess.com' }} />
+            <CSSServerStatus server={{ host: 'css.moodycrew.us' }} />
           </Col>
           <Col>
-            <CSGOServerStatus server={{ host: 'csgo.paradyess.com' }} />
+            <CSGOServerStatus server={{ host: 'csgo.moodycrew.us' }} />
           </Col>
         </Row>
       </Container>
