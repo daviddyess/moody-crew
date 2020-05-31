d9130c1786c982c6ea9b3c8b5fa55037b0d74131
daviddyess <david.dyess@gmail.com>
2020-05-30 19:57:23 -0600

    Updated icons

--- src/icons.js
+++ src/icons.js
@@ -1,11 +1,25 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-// Use the imports and library.add() below to add icons
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+  faChevronLeft,
+  faChevronRight,
+  faUserCircle
+} from '@fortawesome/free-solid-svg-icons';
 
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 
 import { faSteam } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faUserCircle, farHeart, faSteam);
+library.add(
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+  faChevronLeft,
+  faChevronRight,
+  faUserCircle,
+  farHeart,
+  faSteam
+);
 
 export default library;
